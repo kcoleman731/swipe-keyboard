fdb77bbb42ebd5075b34861b803cbfff9880884a
Kevin Coleman <kcoleman731@gmail.com>
2016-08-18 10:32:20 -0700

    Adding docs

--- Staples/STActionInputView.h
+++ Staples/STActionInputView.h
@@ -10,16 +10,35 @@
 
 @class STActionInputView;
 
+/**
+ @abstract The `STActionInputViewDelegate` notifies its receiver when a selection has been made.
+ */
 @protocol STActionInputViewDelegate <NSObject>
 
+/**
+ @abstract Notifies the receiver that an item has been selected.
+ @param actionInputView The `STActionInputView` instance.
+ @return item The `NSString` representing the item that was selected.
+ */
 - (void)actionInputView:(STActionInputView *)actionInputView didSelectItem:(NSString *)item;
 
 @end
 
+/** 
+ @abstract The `STActionInputView` provides an input view that can be used to display dynamic selections to a user in place of a keyboard.
+ */
 @interface STActionInputView : UIView
 
+/**
+ @abstract Initiliazes an `STActionInputView`.
+ @param buttonItems An `NSArray` of `NSString` objects representing the selection items to be displayed in the input view.
+ @return An initialized `STActionInputView`.
+ */
 - (id)initWithButtonItems:(NSArray *)buttonItems;
 
+/**
+ @abstract The delegate object for the input view.
+ */
 @property (nonatomic, weak) id<STActionInputViewDelegate> delegate;
 
 @end
